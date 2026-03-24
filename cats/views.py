2109afd622cb1e4a6a6456deec603b369e7360a4
shipov <shipov.max@icloud.com>
2026-03-24 11:16:22 +0300

    update views.py + add permissions.py

--- cats/views.py
+++ cats/views.py
@@ -1,23 +1,36 @@
 from rest_framework import viewsets
-
+# Импортируем пермишены из нашего нового файла
+from .permissions import OwnerOrReadOnly, ReadOnly
 from .models import Achievement, Cat, User
-
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+    # По умолчанию используем наше правило "Владелец или чтение"
+    permission_classes = (OwnerOrReadOnly,)
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user) 
+        # Автоматически сохраняем текущего пользователя как владельца
+        serializer.save(owner=self.request.user)
+
+    def get_permissions(self):
+        # Если нужно посмотреть детали конкретного котика (action 'retrieve')
+        if self.action == 'retrieve':
+            # Разрешаем доступ всем (ReadOnly)
+            return (ReadOnly(),)
+        # В остальных случаях используем настройки из permission_classes
+        return super().get_permissions()
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    # Здесь права не меняем — действует глобальное IsAuthenticated из settings.py
 
 
 class AchievementViewSet(viewsets.ModelViewSet):
     queryset = Achievement.objects.all()
     serializer_class = AchievementSerializer
+    # Можно добавить OwnerOrReadOnly, если у достижений появится владелец
